3da3309aff97e88717bdcc3fe92e1c48273a27aa
sydnief <sydniefarrell@gmail.com>
2023-02-14 20:25:30 -0500

    created prompt for password length, currently not linked to generate button

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,14 @@
 // Assignment code here
+var passwordLength = prompt("Select an amount of characters between 8 and 128");
+if (passwordLength > 128 || passwordLength < 8 || passwordLength == null){
+   window.alert("Must include number between 8 and 128");
+   } else {
+  include in password;
+   }
+// var passwordUppercase = confirm("Would you like to include Uppercase characters?")
+// var passwordLowecase = confirm("Would you like to include lowercase characters?")
+// var passwordNumeric = confirm("Would you like to include numeric characters?")
+// var passwordSpecial = confirm("Would you like to include special characters?")
 
 
 // Get references to the #generate element
