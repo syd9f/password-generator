6c647d20bf875d51f21da767ee5d924f5be734e7
sydnief <sydniefarrell@gmail.com>
2023-02-14 20:36:38 -0500

    wrote pseudo code for each prompt

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,36 @@
 // Assignment code here
 var passwordLength = prompt("Select an amount of characters between 8 and 128");
 if (passwordLength > 128 || passwordLength < 8 || passwordLength == null){
-   window.alert("Must include number between 8 and 128");
-   } else {
-  include in password;
-   }
-// var passwordUppercase = confirm("Would you like to include Uppercase characters?")
-// var passwordLowecase = confirm("Would you like to include lowercase characters?")
-// var passwordNumeric = confirm("Would you like to include numeric characters?")
-// var passwordSpecial = confirm("Would you like to include special characters?")
+  window.alert("Must include number between 8 and 128");
+  } else {
+  // code to include in password;
+  }
+
+var passwordUppercase = confirm("Would you like to include Uppercase characters?")
+if (passwordUppercase == null) {
+  // code to not include in password;
+} else {
+  // code to include in password;
+}
+
+var passwordLowecase = confirm("Would you like to include lowercase characters?")
+if (passwordUppercase == null) {
+  // code to not include in password;
+} else {
+  // code to include in password;
+}
+var passwordNumeric = confirm("Would you like to include numeric characters?")
+if (passwordUppercase == null) {
+  // code to not include in password;
+} else {
+  // code to include in password;
+}
+var passwordSpecial = confirm("Would you like to include special characters?")
+if (passwordUppercase == null) {
+  // code to not include in password;
+} else {
+  // code to include in password;
+}
 
 
 // Get references to the #generate element
