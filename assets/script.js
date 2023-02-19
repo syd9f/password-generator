150506dbd4bea78d4b02cb712c5a4cc067d0ab89
sydnief <sydniefarrell@gmail.com>
2023-02-19 17:27:41 -0500

    moved write password function into generatePassword function, password now appears on page after generated

--- assets/script.js
+++ assets/script.js
@@ -104,20 +104,14 @@ function generatePassword() {
     randomPassword += charset.charAt(Math.floor(Math.random() * charset.length));
   }
 
-  console.log(randomPassword);
+  // Write password to the #password input
+  var passwordText = document.querySelector("#password");
+  
+  passwordText.value = randomPassword;
 }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
